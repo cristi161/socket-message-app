d8af10c80e10d05571d175bcefdd75608988a295
cristi161 <tomoiucris787@gmail.com>
2020-04-03 21:26:22 +0300

    First demo of message app

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"
 PORT = 5005
@@ -13,13 +14,45 @@ def send_message(sock, message):
     sock.sendall(message)
     return sock.recv(BUFFER)
 
-s = create_sock()
+class InputThread(threading.Thread):
+    def __init__(self, sock):
+        threading.Thread.__init__(self)
+        self.sock = sock
+    def run(self) -> None:
+        message = input()
+        if message == "q":
+            self.sock.close() #a break for while is necessary
+            return None
+        send_message(self.sock, bytes(message, "utf8"))
 
-while True:
-    message = input()
-    if message == "q":
-        s.close()
-        break
-    print(send_message(s, bytes(message, "utf8")))
+class RecvThread(threading.Thread):
+    def __init__(self, sock):
+        threading.Thread.__init__(self)
+        self.sock = sock
+    def run(self) -> None:
+        recv_msg = s.recv(BUFFER).decode("utf-8")
+        print("Received message: " + str(recv_msg))
 
-#s.close()
+if __name__ == '__main__':
+    s = create_sock()
+
+    clients = s.recv(BUFFER).decode("utf-8")
+    clients_nr = len(clients.split(":")) - 1
+
+    if clients_nr == 1:
+        print("No connected users")
+    else:
+        print("Connected users: " + clients)
+
+    while True:
+        input_t = InputThread(s)
+        recv_t = RecvThread(s)
+        input_t.start()
+        recv_t.start()
+
+        input_t.join()
+
+        if input_t == None:
+            break
+
+    s.close()
--- server.py
+++ server.py
@@ -5,15 +5,15 @@ HOST = '127.0.0.1'
 PORT = 5005
 BUFFER_SIZE = 1024
 
-def close_connection(sock, addr):
-    sock.close()
-    print(str(addr) + " has been disconnected")
+connected_clients_ports   = {}
+connected_clients_sockets = {}
 
 class ConnectionThread(threading.Thread):
     def __init__(self, conn, addr):
         threading.Thread.__init__(self)
         self.client_sock = conn
         self.addr = addr
+        self.name = "client_port:" + str(addr[1])
 
     def run(self) -> None:
         message = ""
@@ -22,23 +22,56 @@ class ConnectionThread(threading.Thread):
             if not data:
                 break
             message = message.join(str(data))
-            self.client_sock.send(b"Message received")
+
+            dest, msg = process_message(data.decode("utf-8"))
+            get_sock(dest).sendall(bytes(msg, "utf8"))
+            #self.client_sock.send(b"Message received")
             print(str(self.addr) + " said: " + str(data) + "(" + str(self.name) + ")")
+        connected_clients_ports[addr[1]] = False
         print(str(self.addr) + " has been disconnected")
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
 
-print("Server started")
-print("Waiting for connection...")
+    def bind(self, conn, addr):
+        self.client_sock = conn
+        self.addr = addr
 
-thread_pool = []
-thread_pool_size = 0
+def get_sock(port) -> socket:
+    return connected_clients_sockets[port]
 
-while True:
-    sock.listen(1)
-    conn, addr = sock.accept()
-    newthread = ConnectionThread(conn, addr)
-    newthread.start()
-    print(str(addr) + " is connected")
-    #newthread.join()
-    #conn.close()
+def process_message(msg = ""):
+    message = msg.split("::")
+
+    return int(message[0]), message[1]
+
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((HOST, PORT))
+
+    print("Server started")
+    print("Waiting for connection...")
+
+    #tp = create_thread_pool(20)
+    #tp_size = 0
+
+    while True:
+        clients = ''
+        sock.listen(1)
+        conn, addr = sock.accept()
+
+        if not connected_clients_ports.get(addr[1]):
+            connected_clients_ports[addr[1]] = True
+            connected_clients_ports[str(addr[1])+"_ws"] = conn
+        for key, value in connected_clients_ports.items():
+            if value == True:
+                clients = clients + str(key) + ":"
+
+        conn.sendall(bytes(clients, "utf8"))
+
+        connected_clients_sockets[int(addr[1])] = conn
+
+        newthread = ConnectionThread(conn, addr)
+        newthread.start()
+
+        print(str(addr) + " is connected")
+        #newthread.join()
+        #conn.close()
